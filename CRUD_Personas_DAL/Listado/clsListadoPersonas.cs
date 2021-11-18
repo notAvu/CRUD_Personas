904bdc86411de0a41b15ab6b445868499d2347dc
Alvaro <alvaro.fernandez@iesnervion.es>
2021-11-18 18:14:32 +0100

    Listo 👍

--- CRUD_Personas_DAL/Listado/clsListadoPersonas.cs
+++ CRUD_Personas_DAL/Listado/clsListadoPersonas.cs
@@ -10,17 +10,45 @@ namespace CRUD_Personas_DAL.Listado
     class clsListadoPersonas
     {
         List<clsPersona> listadoCompleto;
+        List<clsPersona> listadoFiltrado;
         clsMyConnection conexion= new clsMyConnection();
 
         public List<clsPersona> ListadoCompleto { get => listadoCompleto; set => listadoCompleto = value; }
 
-        private void executeConnection() {
-            SqlConnection sqlConnection= conexion.getConnection();
+        private void RellenarListado() {
+            LlenarListaElegida(listadoCompleto, "SELECT * FROM Personas");
+        }
+        private void FiltrarPorDepartamento(int idDep)
+        {
+            LlenarListaElegida(listadoFiltrado, $"SELECT * FROM Personas WHERE IDDepartamento={idDep}");
+        }
+        private void LlenarListaElegida(List<clsPersona> listadoElegido, string consultaSql) 
+        {
+            SqlConnection sqlConnection = conexion.getConnection();
             SqlCommand comando = new SqlCommand();
             SqlDataReader reader;
-            comando.CommandText = "SELECT * FROM Personas";
+            comando.CommandText = consultaSql;
             comando.Connection = sqlConnection;
-            
+            reader = comando.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int id = (int)reader["IDPersona"];
+                    string nombre = (string)reader["nombrePersona"];
+                    string apellido = (string)reader["apellidosPersona"];
+                    DateTime dateTime = (DateTime)reader["fechaNacimiento"];
+                    string telefono = (string)reader["telefono"];
+                    string direccion = (string)reader["direccion"];
+                    string fotoUrl = (string)reader["foto"];
+                    int idDepartamento = (int)reader["IDDepartamento"];
+                    clsPersona oPersona = new clsPersona(id, nombre, apellido, dateTime, telefono, direccion, fotoUrl, idDepartamento);
+                    listadoElegido.Add(oPersona);
+                }
+            }
+            reader.Close();
+            conexion.closeConnection(ref sqlConnection);
         }
     }
+    
 }
