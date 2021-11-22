5f040e0884ddc07ea7e02e99d99ce6376094e4fb
Alvaro <alvaro.fernandez@iesnervion.es>
2021-11-22 14:16:26 +0100

    Trabajando en la vista principal y Personas

---
+++ CRUD_Personas_UWP/ViewModels/PersonasVM.cs
@@ -0,0 +1,22 @@
+﻿using CRUD_Personas_DAL.Listado;
+using Entities_UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Personas_UWP.ViewModels
+{
+    public class PersonasVM
+    {
+        List<clsPersona> listadoPersonas;
+        clsPersona personaSeleccionada;
+
+        public PersonasVM(List<clsPersona> listadoPersonas)
+        {
+            this.listadoPersonas = new clsListadoPersonas().ListadoCompleto;
+            personaSeleccionada = listadoPersonas[0];
+        }
+    }
+}
