b352a58ff90720f4ab5186c62a34476d3e3290f2
Álvaro Fernández <avunotfound@gmail.com>
2021-11-25 10:03:50 +0100

    Que plan con el model este

---
+++ CRUD_Personas_ASP/Models/clsPersonaConNombreDepartamento.cs
@@ -0,0 +1,22 @@
+﻿using CRUD_Personas_BL;
+using CRUD_Personas_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Personas_ASP.Models
+{
+    public class clsPersonaConNombreDepartamento
+    {
+        clsPersona persona;
+        string nombreDepartamento;
+        PersonasBL gestoraPersonas;
+        DepartamentosBL gestoraDepartamentos;
+        public clsPersonaConNombreDepartamento()
+        {
+            gestoraPersonas = new PersonasBL();
+            gestoraDepartamentos = new DepartamentosBL();
+        }
+    }
+}
